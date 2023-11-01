f19687476702117700932512500f232c64233208
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-02 00:02:54 +0100

    - Mise a jour du Service api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://api.aventure-en-adra.fr/', // URL de base de votre API Laravel
+    baseURL: 'https://api.aventure-en-adra.fr', // URL de base de votre API Laravel
     withCredentials: true // Important pour envoyer et recevoir des cookies
 });
 
