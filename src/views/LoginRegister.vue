7c73f77e636f037b3388d838fbc9848a7c231d4a
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-02 10:27:39 +0100

    - Mise a jour des vue

---
+++ src/views/LoginRegister.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <Login/>
+      <Register/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Login from '@/components/Login.vue';
+import Register from '@/components/Register.vue';
+
+</script>
