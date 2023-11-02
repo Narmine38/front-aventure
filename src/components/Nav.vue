34afa61b86d18819418799e2669a7d598c1913f5
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-02 10:29:10 +0100

    - Ajout du Composant Nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav class="nav">
+    <!-- Logo sur la gauche -->
+    <div class="logo">
+      <img src="@/assets/your-logo-path.png" alt="Mon Logo">
+    </div>
+
+    <!-- Dropdown "Mon Compte" sur la droite -->
+    <!-- Dropdown "Mon Compte" ou redirection vers "register" -->
+    <div v-if="isLoggedIn" class="account-dropdown" @click="toggleDropdown">
+      Mon compte
+      <div v-if="showDropdown" class="dropdown-content">
+        <router-link to="/mon-compte">Profil</router-link>
+        <button @click.prevent="logout" class="logout-button">Déconnexion</button>
+      </div>
+    </div>
+
+    <div v-else class="register-link" @click="goToRegisterOrLogin">
+      Mon compte
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import {computed, ref} from 'vue';
+import {useAuthStore} from '/src/stores/authStore.js'; // Si vous utilisez Vuex
+import {useRouter} from 'vue-router';
+
+const store = useAuthStore();
+const router = useRouter();
+const showDropdown = ref(false);
+
+const isLoggedIn = computed(() => store.isLoggedIn);
+const logout = computed(() => store.logout())
+
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value;
+};
+
+const goToRegisterOrLogin = () => {
+  router.push({name: 'Connexion-inscription'});
+};
+
+
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+}
+
+.account-dropdown {
+  position: relative;
+  cursor: pointer;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  right: 0;
+  top: 100%;
+  border: 1px solid #ccc;
+  background-color: #f9f9f9;
+  z-index: 1;
+}
+
+.account-dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
