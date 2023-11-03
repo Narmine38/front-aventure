22704405beba5e5f16695621ea5958651ffd1234
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-04 00:09:39 +0100

    - Mise a jour des routes

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,43 @@ const router = createRouter({
       name: 'mon-compte',
       component: () => import('../views/MonCompte.vue')
     },
+    {
+      path: '/administration',
+      name: 'administration',
+      component: () => import('../views/admin/Administration.vue'),
+      children: [
+        {
+          path: 'usersmanagement',  // ceci est relatif à '/administration'
+          name: 'usersmanagement',
+          component: () => import('../views/admin/UsersManagement.vue')
+        },
+        {
+          path: 'placesmanagement',  // ceci est relatif à '/administration'
+          name: 'placesmanagement',
+          component: () => import('../views/admin/PlacesManagement.vue')
+        },
+        {
+          path: 'accommodationsmanagement',  // ceci est relatif à '/administration'
+          name: 'accommodationsmanagement',
+          component: () => import('../views/admin/AccommodationsManagement.vue')
+        },
+        {
+          path: 'activitiesmanagement',  // ceci est relatif à '/administration'
+          name: 'activitiesmanagement',
+          component: () => import('../views/admin/ActivitesManagement.vue')
+        },
+        {
+          path: 'charactersmanagement',  // ceci est relatif à '/administration'
+          name: 'charactersmanagement',
+          component: () => import('../views/admin/PersonnageManagement.vue')
+        },
+        {
+          path: 'reservationsmanagement',  // ceci est relatif à '/administration'
+          name: 'reservationsmanagement',
+          component: () => import('../views/admin/ReservationManagement.vue')
+        },
+      ]
+    },
   ]
 })
 
