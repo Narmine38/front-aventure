1629d4a174b030203c7bbbed05e26c1aca9f39a7
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-03 22:00:58 +0100

    - Mise a jour du service

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const api = axios.create({
     baseURL: 'https://api.aventure-en-adra.fr', // URL de base de votre API Laravel
     withCredentials: true // Important pour envoyer et recevoir des cookies
 });
 
-export default instance;
+// Fonction pour définir l'en-tête d'autorisation.
+function setAuthorizationHeader() {
+    const token = sessionStorage.getItem('auth_token');
+    if (token) {
+        api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+    }
+}
+
+// Vous pouvez aussi choisir de définir l'en-tête immédiatement si le token existe déjà
+setAuthorizationHeader();
+
+export default api;
+
+
+
+
+
