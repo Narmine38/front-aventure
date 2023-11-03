d9b923715f4c774469d86ee28e297061c9feef99
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-03 13:57:33 +0100

    - Mise a jour Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,79 +1,74 @@
 <template>
-  <div>
-      <input v-model="email" placeholder="Email">
-      <input v-model="password" placeholder="Password" type="password">
-      <button @click="attemptLogin">Login</button>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div>
+        <label for="email">Email:</label>
+        <input id="email" type="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input id="password" type="password" v-model="password" required>
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+    </form>
   </div>
+  <button @click="logout">Logout</button>
+  <p v-if="message">{{ message }}</p>
+
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { useAuthStore } from '@/stores/authStore';
-import router from "@/router";
+import axios from 'axios';
 
-const authStore = useAuthStore();
 const email = ref('');
 const password = ref('');
+const errorMessage = ref('');
+const message = ref('');
 
-const attemptLogin = async () => {
-  await authStore.login({ email: email.value, password: password.value });
-  await router.push("/");
+
+const login = async () => {
+  try {
+    const response = await axios.post('http://127.0.0.1:8000/api/login', {
+      email: email.value,
+      password: password.value,
+    });
+    console.log(response.data);
+    localStorage.setItem('token', response.data.access_token);
+    // Handle your logic after successful login, like redirecting the user
+  } catch (error) {
+    if (error.response) {
+      errorMessage.value = error.response.data.error || 'Invalid credentials.';
+    } else {
+      errorMessage.value = 'The login process could not be completed.';
+    }
+  }
 };
 
-
-
-const isLoggedIn = ref(authStore.isLoggedIn);
+const logout = async () => {
+  try {
+    const response = await axios.post('http://127.0.0.1:8000/api/logout', {}, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    message.value = response.data.message;
+    localStorage.removeItem('token');
+    // Rediriger l'utilisateur ou mettre à jour l'état de l'application après la déconnexion
+  } catch (error) {
+    message.value = 'Failed to logout.';
+    console.error('Logout error:', error);
+  }
+};
 </script>
 
 <style scoped>
-/* Fond d'écran */
-div {
-  /* Couleur de fond d'un vieux parchemin */
-  background: #F4E9D8 ;
-  background-size: cover;
-  padding: 2rem;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  max-width: 400px;
-  margin: auto;
-  border: 3px solid #7B5E47; /* Bordure marron rappelant le cuir */
-}
-
-/* Style de base pour les boutons et champs de saisie */
-button, input {
-  font-family: 'Ringbearer', sans-serif; /* Une police rappelant l'écriture du "Seigneur des Anneaux" */
-  border: 1px solid #7B5E47;
-  padding: 10px;
-  margin: 5px;
-  border-radius: 5px;
-  background-color: transparent;
-  color: #3E2723; /* Marron foncé pour un look ancien */
-  font-size: 16px;
-}
-
-button:hover {
-  background-color: #BFAE96; /* Marron clair au survol */
-  cursor: pointer;
-}
-
-input::placeholder {
-  color: #7B5E47; /* Couleur de placeholder marron */
-}
-
-/* Ornementations */
-div:before, div:after {
-  content: "";
-  display: block;
-  width: 50px;
-  height: 50px;
-  margin: auto;
-}
-
-div:before {
-  margin-bottom: 20px;
-}
-
-div:after {
-  margin-top: 20px;
+.login-container {
+  /* Same styling as Register.vue */
+  /* ... */
 }
 </style>
