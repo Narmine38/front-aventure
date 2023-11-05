3bc4f942526e45355bdb8693152fbcabaeffc97d
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-05 17:37:07 +0100

    - Ajout de commentaires

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,7 +1,7 @@
 // Importation des dépendances nécessaires
 import { defineStore } from 'pinia';
-import api from "@/services/api";
-import router from "@/router";
+import api from "@/services/api"; // API pour les requêtes HTTP
+import router from "@/router"; // Router pour la navigation
 
 // Définition du store d'authentification avec Pinia
 export const useAuthStore = defineStore({
@@ -10,48 +10,48 @@ export const useAuthStore = defineStore({
 
     // Initialisation de l'état avec les données de session existantes ou des valeurs par défaut
     state: () => ({
-        user: JSON.parse(sessionStorage.getItem('user')) || null,
-        token: sessionStorage.getItem('token') || '',
-        roles: JSON.parse(sessionStorage.getItem('roles')) || []
+        user: JSON.parse(sessionStorage.getItem('user')) || null, // L'utilisateur actuellement connecté
+        token: sessionStorage.getItem('token') || '', // Token d'authentification
+        roles: JSON.parse(sessionStorage.getItem('roles')) || [] // Rôles de l'utilisateur
     }),
 
     // Les getters fournissent une manière réactive d'accéder à des dérivations de l'état du store
     getters: {
-        isLoggedIn: (state) => !!state.token, // Renvoie vrai si un token est présent
-        isAdmin: (state) => state.roles.includes('admin') // Vérifie si l'utilisateur est un admin
+        isLoggedIn: (state) => !!state.token, // Renvoie vrai si un token est stocké, donc l'utilisateur est considéré comme connecté
+        isAdmin: (state) => state.roles.includes('admin') // Vérifie si parmi les rôles de l'utilisateur se trouve 'admin'
     },
 
     // Les actions contiennent la logique pour les opérations telles que le login, logout, etc.
     actions: {
-        // Définit l'entête d'autorisation pour les requêtes HTTP sortantes
+        // Définit l'entête d'autorisation pour les requêtes HTTP sortantes avec le token JWT
         setAuthorizationHeader() {
             const token = this.token;
             if (token) {
-                api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                api.defaults.headers.common['Authorization'] = `Bearer ${token}`; // Ajout du token au header par défaut pour toutes les requêtes suivantes
             }
         },
 
-        // Prépare les requêtes authentifiées en obtenant un cookie CSRF
+        // Prépare les requêtes authentifiées en obtenant un cookie CSRF (nécessaire pour la protection contre les attaques CSRF)
         async prepareForAuthRequest() {
-            await api.get('/sanctum/csrf-cookie');
+            await api.get('/sanctum/csrf-cookie'); // Obtention du cookie CSRF avant la soumission des données de connexion
         },
 
         // Gère la logique de connexion de l'utilisateur
         async login(email, password) {
             await this.prepareForAuthRequest(); // Préparation pour la requête CSRF
             try {
-                const response = await api.post('/api/login', { email, password });
-                // Stockage du token, de l'utilisateur et des rôles dans le state et sessionStorage
+                const response = await api.post('/api/login', { email, password }); // Tentative de connexion avec email et mot de passe
+                // En cas de réussite, mise à jour du state et de sessionStorage avec le token, l'utilisateur et ses rôles
                 this.token = response.data.access_token;
                 this.user = response.data.user;
                 this.roles = response.data.roles;
-                sessionStorage.setItem('token', this.token);
-                sessionStorage.setItem('user', JSON.stringify(this.user));
-                sessionStorage.setItem('roles', JSON.stringify(this.roles));
+                sessionStorage.setItem('token', this.token); // Stockage du token en session pour une utilisation ultérieure
+                sessionStorage.setItem('user', JSON.stringify(this.user)); // Stockage de l'utilisateur en session
+                sessionStorage.setItem('roles', JSON.stringify(this.roles)); // Stockage des rôles en session
 
                 await router.push('/'); // Redirection à la page d'accueil après le login
             } catch (error) {
-                // Affichage et gestion des erreurs de connexion
+                // En cas d'erreur, un message est affiché dans la console
                 console.error('Login error:', error);
             }
         },
@@ -59,20 +59,20 @@ export const useAuthStore = defineStore({
         // Gère la logique de déconnexion de l'utilisateur
         async logout() {
             await this.prepareForAuthRequest(); // Même préparation pour la requête CSRF
-            this.setAuthorizationHeader();
+            this.setAuthorizationHeader(); // Configuration de l'entête d'autorisation avec le token actuel
             try {
                 await api.post('/api/logout'); // Appel API pour déconnecter l'utilisateur
-                // Nettoyage du state et du sessionStorage
+                // Réinitialisation du state et du sessionStorage
                 this.token = '';
                 this.user = null;
                 this.roles = [];
-                sessionStorage.clear(); // Alternative pour supprimer tous les items de session
+                sessionStorage.clear(); // Efface tous les items enregistrés dans le sessionStorage
 
                 delete api.defaults.headers.common['Authorization']; // Suppression de l'entête d'autorisation
 
                 await router.push('/login'); // Redirection vers la page de connexion
             } catch (error) {
-                // Affichage et gestion des erreurs de déconnexion
+                // En cas d'erreur de déconnexion, un message est affiché dans la console
                 console.error('Logout error:', error);
             }
         },
@@ -81,12 +81,12 @@ export const useAuthStore = defineStore({
         async register(userData) {
             await this.prepareForAuthRequest(); // Utilise la même préparation pour CSRF que login et logout
             try {
-                const response = await api.post('/api/register', userData);
-                // Logique après une inscription réussie, peut inclure la configuration de l'état de l'utilisateur si nécessaire
+                const response = await api.post('/api/register', userData); // Envoi des données d'inscription à l'API
+                // Traitement après une inscription réussie
                 console.log('Registration successful:', response.data);
-                await router.push('/'); // Redirection après inscription
+                await router.push('/'); // Redirection à la page d'accueil ou au tableau de bord de l'utilisateur
             } catch (error) {
-                // Gestion des erreurs d'inscription avec des détails spécifiques
+                // En cas d'erreur d'inscription, affiche les détails spécifiques dans la console
                 console.error("Registration error:", error.response.data);
             }
         },
--- src/stores/userProfileStore.js
+++ src/stores/userProfileStore.js
@@ -1,59 +1,72 @@
-import {defineStore} from 'pinia';
+import { defineStore } from 'pinia';
 import api from '@/services/api';
-import {useAuthStore} from "@/stores/authStore";
+import { useAuthStore } from "@/stores/authStore";
 import router from "@/router";
 
 export const useUserProfileStore = defineStore('userProfile', {
     state: () => ({
+        // L'état 'user' stocke les informations de l'utilisateur actuellement connecté.
+        // Il tente de récupérer ces informations du sessionStorage lors de l'initialisation.
         user: JSON.parse(sessionStorage.getItem('user')) || null,
+        // L'état 'userReservations' pour stocker les réservations de l'utilisateur.
         userReservations: [],
     }),
     getters: {
-        // Définissez ici les getters pour accéder aux états calculés ou filtrés
+        // Définissez ici les getters si vous avez besoin d'accéder à des états calculés basés sur l'état actuel.
     },
     actions: {
+        // Action pour récupérer le profil de l'utilisateur en fonction de l'ID.
+        // Il met à jour l'état 'user' et stocke les informations dans sessionStorage.
         async fetchUserProfile(id) {
-            const authStore = useAuthStore(); // Utilisez authStore ici
-            authStore.setAuthorizationHeader();
+            const authStore = useAuthStore(); // Utilisation du store d'authentification pour les actions nécessitant une authentification.
+            authStore.setAuthorizationHeader(); // Définit le header d'autorisation pour les requêtes API suivantes.
             try {
+                // Effectue une requête GET pour récupérer le profil utilisateur.
                 const response = await api.get(`/api/users/${id}`);
-                this.user = response.data.data;
+                this.user = response.data.data; // Met à jour les données de l'utilisateur dans le state.
+                // Sauvegarde les données de l'utilisateur dans sessionStorage pour un accès rapide et persistant.
                 sessionStorage.setItem('user', JSON.stringify(this.user));
             } catch (error) {
+                // En cas d'erreur, l'état 'error' est mis à jour avec un message d'erreur.
                 this.error = "Erreur lors de la récupération du profil de l'utilisateur";
-                // Gérez l'erreur comme il se doit
             }
         },
 
-
+        // Action pour archiver un utilisateur et déconnecter la session.
         async archiveUser(id) {
-            const authStore = useAuthStore(); // Utilisez authStore ici
+            const authStore = useAuthStore();
+            await authStore.prepareForAuthRequest(); // Préparation pour une requête authentifiée si nécessaire.
             authStore.setAuthorizationHeader();
             try {
+                // Requête POST pour archiver un utilisateur.
                 await api.post(`/api/users/${id}/archive`);
-                await authStore.logout();
-                await router.push('/connexion')
-                window.location.reload()
+                await authStore.logout(); // Déconnecte l'utilisateur après l'archivage.
+                await router.push('/connexion'); // Redirige vers la page de connexion.
+                window.location.reload(); // Recharge la fenêtre pour nettoyer l'état de l'application.
             } catch (error) {
+                // Gestion des erreurs lors de l'archivage de l'utilisateur.
                 this.error = "Erreur lors de l'archivage de l'utilisateur";
             }
         },
 
-
+        // Action pour mettre à jour le profil utilisateur avec de nouvelles données.
         async updateUserProfile(updatedUserData) {
-            const authStore = useAuthStore(); // Utilisez authStore ici
+            const authStore = useAuthStore();
+            await authStore.prepareForAuthRequest();
             authStore.setAuthorizationHeader();
             try {
+                // Requête PUT pour mettre à jour les informations de l'utilisateur.
                 const response = await api.put(`/api/users/${updatedUserData.id}`, updatedUserData);
-                this.user = response.data.data; // Assurez-vous que ceci correspond à l'état déclaré
+                this.user = response.data.data; // Met à jour l'état 'user' avec les nouvelles informations.
+                // Met à jour sessionStorage avec les nouvelles données utilisateur.
                 sessionStorage.setItem('user', JSON.stringify(this.user));
+                // Retourne un message de réussite de la mise à jour.
                 return response.data.message;
             } catch (error) {
+                // Lance une exception en cas d'erreur lors de la mise à jour des données de l'utilisateur.
                 this.error = "Erreur lors de la mise à jour du profil de l'utilisateur";
-                throw error;
+                throw error; // Renvoie l'erreur pour une gestion externe.
             }
         },
-
-
     }
 });
