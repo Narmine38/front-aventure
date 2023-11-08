a3385dd6c3c103d26d32ba321c9007ef93759b68
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-08 11:56:12 +0100

    - Mise a jour de de CharacterCard.vue et sont components

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,10 +1,81 @@
 <template>
-
+  <div class="character-card">
+    <div class="character-image">
+      <!-- Assurez-vous que le lien vers la photo est complet et valide -->
+      <img :src="character.picture" :alt="character.name" />
+    </div>
+    <div class="character-info">
+      <h3 class="character-name">{{ character.name }}</h3>
+      <p class="character-story">{{ character.story }}</p>
+    </div>
+    <div class="character-place-info">
+      <p class="place-name">{{ character.place.name }}</p>
+      <p class="place-description">{{ character.place.shortDescription }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
 
+defineProps({
+  character: {
+    type: Object,
+    required: true
+  }
+});
 </script>
 
 <style scoped>
+.character-card {
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+  transition: box-shadow 0.3s ease;
+  background: white;
+  margin-bottom: 1rem;
+}
+
+.character-card:hover {
+  box-shadow: 0 4px 12px rgba(0,0,0,0.15);
+}
+
+.character-image img {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+
+.character-info {
+  padding: 1rem;
+}
+
+.character-name {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.character-story {
+  font-size: 0.9rem;
+  color: #666;
+  margin-bottom: 0.5rem;
+}
+
+.character-place-info {
+  padding: 0 1rem 1rem;
+  background: #f8f8f8;
+  border-top: 1px solid #eee;
+}
+
+.place-name {
+  font-weight: bold;
+  color: #333;
+}
+
+.place-description {
+  font-size: 0.8rem;
+  color: #666;
+}
 </style>
--- src/views/Charater.vue
+++ src/views/Charater.vue
@@ -1,10 +1,48 @@
 <template>
-
+  <section class="characters-section">
+    <h1>Rencontrez les personnages de la Terre du Milieu</h1>
+    <div class="characters-container">
+      <CharacterCard
+          v-for="character in characters"
+          :key="character.id"
+          :character="character"
+      />
+    </div>
+  </section>
 </template>
 
 <script setup>
+import CharacterCard from "@/components/CharacterCard.vue";
+import { ref, onMounted } from 'vue';
+import { useCharactersStore } from "/src/stores/CharactersStore";
+const charactersStore = useCharactersStore();
+const characters = ref([]);
 
+onMounted(async () => {
+  await charactersStore.fetchCharaters()
+  characters.value = charactersStore.characters;
+});
 </script>
 
 <style scoped>
+.characters-section {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+.characters-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  justify-content: center;
+}
+
+@media (max-width: 768px) {
+  .characters-container {
+    flex-direction: column;
+    align-items: center;
+  }
+}
 </style>
