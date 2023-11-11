9ebb2238dadfe3affe7b77f566f63e0610ebba54
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-11 08:28:12 +0100

    - Mise a jour

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import ComponanteNav from "@/components/HeaderComponante.vue";
+import 'bootstrap/dist/css/bootstrap.min.css'
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 const app = createApp(App)
