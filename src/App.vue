765f8d1c2fffa84ca44026c8ff38e1a4ea199aae
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-11-11 08:32:02 +0100

    - Mise a jour

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import ComponanteNav from "@/components/HeaderComponante.vue";
-import 'bootstrap/dist/css/bootstrap.min.css'
 </script>
 
 <template>
